559ab6570cd991783440c0115d1a1a5812350a9b
João Otávio <joaootavioocortes@gmail.com>
2024-09-03 22:25:29 -0300

    Criando a classe calculadoraImpostoDeRenda

--- ImpostoDeRenda/Program.cs
+++ ImpostoDeRenda/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ImpostoDeRenda.uteis;
+
+Console.WriteLine("Sejam Bem vindos ao seu adiantador de tempo na hora de pagar seus impostos, mesmo que seja roubo.");
+Console.WriteLine("0 - Sair\n1 - Fazer Imposto de Renda\n2 - Torre Hanoi O que deseja fazer?: ");
+int opt;
+do
+{
+    opt = int.Parse(Console.ReadLine());
+    switch (opt)
+    {
+        case 0:
+            opt = 0;
+            break;
+
+        case 1:
+            break;
+
+        case 2:
+            break;
+
+        case 3:
+            break;
+
+    }
+} while (opt != 0);
