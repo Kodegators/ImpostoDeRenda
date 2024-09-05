10b0a5aa2bb8f7c3c49d4066d0d40c6435be3c11
João Otávio <joaootavioocortes@gmail.com>
2024-09-04 23:30:22 -0300

    Fazendo interação no Menu e passando o método CalcularImposto para retornavel

--- ImpostoDeRenda/uteis/CalculadoraImpostoDeRenda.cs
+++ ImpostoDeRenda/uteis/CalculadoraImpostoDeRenda.cs
@@ -8,32 +8,31 @@ namespace ImpostoDeRenda.uteis
 {
     public class CalculadoraImpostoDeRenda
     {
-        public static double CalcularImposto(double renda)
+        public double ValorImposto;
+        public double CalcularImposto(double renda)
         {
-            double imposto = 0.0;
-
             if (renda <= 2259.20)
             {
-                imposto = 0.0;
+                ValorImposto = 0.0;
             }
             else if (renda > 2259.20 && renda <= 2826.65)
             {
-                imposto = renda * 0.075;
+                ValorImposto = renda * 0.075;
             }
             else if (renda > 2826.65 && renda <= 3751.05)
             {
-                imposto = renda * 0.150;
+                ValorImposto = renda * 0.150;
             }
             else if (renda > 3751.05 && renda <= 4664.68)
             {
-                imposto = renda * 0.225;
+                ValorImposto = renda * 0.225;
             }
             else
             {
-                imposto = renda * 0.275;
+                ValorImposto = renda * 0.275;
             }
 
-            return imposto;
+            return ValorImposto;
         }
     }
 }
--- TestProject1/CalcularImpostoTeste.cs
+++ TestProject1/CalcularImpostoTeste.cs
@@ -9,50 +9,50 @@ namespace TestProject1
         public void Faixa1()
         {
             double rendaBruta = 2250.00;
-            double imposto;
-            imposto = CalculadoraImpostoDeRenda.CalcularImposto(rendaBruta);
+            CalculadoraImpostoDeRenda imposto = new CalculadoraImpostoDeRenda();
+            imposto.CalcularImposto(rendaBruta);
 
-            Assert.AreEqual(0.0, imposto);
+            Assert.AreEqual(0.0, imposto.ValorImposto);
         }
 
         [TestMethod]
         public void Faixa2()
         {
             double rendaBruta = 2750.00;
-            double imposto;
-            imposto = CalculadoraImpostoDeRenda.CalcularImposto(rendaBruta);
+            CalculadoraImpostoDeRenda imposto = new CalculadoraImpostoDeRenda();
+            imposto.CalcularImposto(rendaBruta);
 
-            Assert.AreEqual(206.25, imposto);
+            Assert.AreEqual(206.25, imposto.ValorImposto);
         }
 
         [TestMethod]
         public void Faixa3()
         {
             double rendaBruta = 3250.00;
-            double imposto;
-            imposto = CalculadoraImpostoDeRenda.CalcularImposto(rendaBruta);
+            CalculadoraImpostoDeRenda imposto = new CalculadoraImpostoDeRenda();
+            imposto.CalcularImposto(rendaBruta);
 
-            Assert.AreEqual(487.5, imposto);
+            Assert.AreEqual(487.5, imposto.ValorImposto);
         }
 
         [TestMethod]
         public void Faixa4()
         {
             double rendaBruta = 4250.00;
-            double imposto;
-            imposto = CalculadoraImpostoDeRenda.CalcularImposto(rendaBruta);
+            CalculadoraImpostoDeRenda imposto = new CalculadoraImpostoDeRenda();
+            imposto.CalcularImposto(rendaBruta);
 
-            Assert.AreEqual(956.25, imposto);
+            Assert.AreEqual(956.25, imposto.ValorImposto);
         }
 
         [TestMethod]
         public void Faixa5()
         {
             double rendaBruta = 8775.40;
-            double imposto;
-            imposto = CalculadoraImpostoDeRenda.CalcularImposto(rendaBruta);
+            CalculadoraImpostoDeRenda imposto = new CalculadoraImpostoDeRenda();
+            imposto.CalcularImposto(rendaBruta);
 
-            Assert.AreEqual(2413.235, imposto);
+            Assert.AreEqual(2413.235, imposto.ValorImposto);
         }
     }
 }
