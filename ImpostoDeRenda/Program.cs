808f535a5934db05515b04ef8ff7af8f259c8ed4
RodegheriLucas <orodegherilucas@gmail.com>
2024-09-06 23:16:37 -0300

    Finalizando Torres de Hanoi.

--- ImpostoDeRenda/Program.cs
+++ ImpostoDeRenda/Program.cs
@@ -1,8 +1,18 @@
 ﻿using ImpostoDeRenda.uteis;
+using System.Runtime.ConstrainedExecution;
 
 Console.WriteLine("Sejam Bem vindos ao seu adiantador de tempo na hora de pagar seus impostos, mesmo que seja roubo.");
 Console.WriteLine("0 - Sair\n1 - Fazer Imposto de Renda\n2 - Torre Hanoi O que deseja fazer?: ");
 int opt;
+string Final;
+string TiraDisco = "0";
+string ColocaDisco = "0";
+List<List<int>> TorreFinal = new List<List<int>>();
+List<int> TorreWin = new List<int> { 10, 8, 6, 4, 2 };
+List<int> Torre1 = new List<int> { 10, 8, 6, 4, 2 };
+List<int> Torre2 = new List<int> { };
+List<int> Torre3 = new List<int> { };
+
 do
 {
     opt = int.Parse(Console.ReadLine());
@@ -16,7 +26,36 @@ do
             break;
 
         case 2:
-            TorresHanoi.TorresHanoiJogo();
+            List<int> torre1 = new List<int>(TorreFinal[0]);
+            List<int> torre2 = new List<int>(TorreFinal[1]);
+            List<int> torre3 = new List<int>(TorreFinal[2]);
+            var disc = '-';
+            int maxWidth = 10;
+            Console.WriteLine("  1             2            3");
+            for (int j = 5; j >= 0; j--)
+            {
+                int t1 = torre1.ElementAtOrDefault(j);
+                int t2 = torre2.ElementAtOrDefault(j);
+                int t3 = torre3.ElementAtOrDefault(j);
+                string torre1Discs = new string(' ', (maxWidth - t1) / 2) + new string(disc, t1) + new string(' ', (maxWidth - t1 + 1) / 2);
+                string torre2Discs = new string(' ', (maxWidth - t2) / 2) + new string(disc, t2) + new string(' ', (maxWidth - t2 + 1) / 2);
+                string torre3Discs = new string(' ', (maxWidth - t3) / 2) + new string(disc, t3) + new string(' ', (maxWidth - t3 + 1) / 2);
+                Console.WriteLine($"{torre1Discs}   {torre2Discs}   {torre3Discs}");
+            }
+            try
+            {
+                do
+                {
+                    EscolhaHanoi();
+                    TorreFinal = TorresHanoi.MetodoConstroiTorre(TiraDisco, ColocaDisco, Torre1, Torre2, Torre3);
+                    Final = TorresHanoi.TorresHanoiJogo(TorreFinal, TorreWin);
+                } while (Final != "0");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Movimento Invalido");
+                goto case 2;
+            }
             break;
 
         case 3:
@@ -24,3 +63,12 @@ do
 
     }
 } while (opt != 0);
+
+void EscolhaHanoi()
+{
+    Console.Write("De qual torre você deseja remover o disco?: ");
+    TiraDisco = Console.ReadLine();
+    Console.Write("Deseja colocar o disco em qual torre?: ");
+    ColocaDisco = Console.ReadLine();
+
+}
--- ImpostoDeRenda/uteis/TorresHanoi.cs
+++ ImpostoDeRenda/uteis/TorresHanoi.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,20 +9,145 @@ namespace ImpostoDeRenda.uteis
 {
     public class TorresHanoi
     {
-
-        public static void TorresHanoiJogo()
+        public static string TorresHanoiJogo(List<List<int>> TorreFinal, List<int> TorreWin)
         {
-            var disc = '-';
-            List<int> torre1 = new List<int> { 1, 2, 3, 4, 5 };
-            List<int> torre2 = new List<int> { };
-            List<int> torre3 = new List<int> { };
 
-            for (int i = 0; i < torre1.Count; i++)
+            List<int> torre1 = new List<int>(TorreFinal[0]);
+            List<int> torre2 = new List<int>(TorreFinal[1]);
+            List<int> torre3 = new List<int>(TorreFinal[2]);
+            var disc = '-';
+            int maxWidth = 10;
+            Console.WriteLine("  1             2            3");
+
+
+            for (int i = 5; i >= 0; i--)
             {
-                int t2 = torre2.ElementAtOrDefault(i);
-                int t3 = torre3.ElementAtOrDefault(i);
-                Console.WriteLine(new string(disc, torre1[i]));
+                if (torre1.Count == 0)
+                {
+                    for (int j = 5; j >= 0; j--)
+                    {
+                        int t1 = torre1.ElementAtOrDefault(j);
+                        int t2 = torre2.ElementAtOrDefault(j);
+                        int t3 = torre3.ElementAtOrDefault(j);
+                        string torre1Discs = new string(' ', (maxWidth - t1) / 2) + new string(disc, t1) + new string(' ', (maxWidth - t1 + 1) / 2);
+                        string torre2Discs = new string(' ', (maxWidth - t2) / 2) + new string(disc, t2) + new string(' ', (maxWidth - t2 + 1) / 2);
+                        string torre3Discs = new string(' ', (maxWidth - t3) / 2) + new string(disc, t3) + new string(' ', (maxWidth - t3 + 1) / 2);
+                        Console.WriteLine($"{torre1Discs}   {torre2Discs}   {torre3Discs}");
+                    }
+                    if (torre2 == TorreWin || torre3 == TorreWin)
+                        return "0";
+                }
+                else
+                {
+                    int t1 = torre1.ElementAtOrDefault(i);
+                    int t2 = torre2.ElementAtOrDefault(i);
+                    int t3 = torre3.ElementAtOrDefault(i);
+                    string torre1Discs = new string(' ', (maxWidth - t1) / 2) + new string(disc, t1) + new string(' ', (maxWidth - t1 + 1) / 2);
+                    string torre2Discs = new string(' ', (maxWidth - t2) / 2) + new string(disc, t2) + new string(' ', (maxWidth - t2 + 1) / 2);
+                    string torre3Discs = new string(' ', (maxWidth - t3) / 2) + new string(disc, t3) + new string(' ', (maxWidth - t3 + 1) / 2);
+                    Console.WriteLine($"{torre1Discs}   {torre2Discs}   {torre3Discs}");
+                }
             }
+            return "1";
         }
+
+        public static List<List<int>> MetodoConstroiTorre(string tira, string coloca, List<int> torre1, List<int> torre2, List<int> torre3)
+        {
+            
+            List<List<int>> TorreFinal = new List<List<int>>();
+            int Remvd = 0;
+            switch (tira)
+            {
+                case "1":
+                    Remvd = torre1[torre1.Count - 1];
+                    torre1.Remove(torre1[torre1.Count - 1]);
+                    break;
+                case "2":
+                    Remvd = torre2[torre2.Count - 1];
+                    torre2.Remove(torre2[torre2.Count - 1]);
+                    break;
+                case "3":
+                    Remvd = torre3[torre3.Count - 1];
+                    torre3.Remove(torre3[torre3.Count - 1]);
+                    break;
+            }
+
+            switch (coloca)
+            {
+                case "1":
+                    if (torre1.Count == 0)
+                    {
+                        torre1.Add(Remvd);
+                    }
+                    else if (torre1[torre1.Count - 1] > Remvd)
+                    {
+                        torre1.Add(Remvd);
+                    }
+                    else
+                    {
+                        if (tira == "2")
+                        {
+                            goto case "2";
+                        }
+                        else
+                        {
+                            goto case "3";
+                        }
+                        throw new ArithmeticException("Movimento Invalido");
+                    }
+                    break;
+                case "2":
+                    if (torre2.Count == 0)
+                    {
+                        torre2.Add(Remvd);
+                    }
+                    else if (torre2[torre2.Count - 1] > Remvd)
+                    {
+                        torre2.Add(Remvd);
+                    }
+                    else
+                    {
+                        if (tira == "1")
+                        {
+                            goto case "1";
+                        }
+                        else
+                        {
+                            goto case "3";
+                        }
+                        throw new ArithmeticException("Movimento Invalido");
+                    }
+                    break;
+                case "3":
+                    if (torre3.Count == 0)
+                    {
+                        torre3.Add(Remvd);
+                    }
+                    else if (torre3[torre3.Count - 1] > Remvd)
+                    {
+                        torre3.Add(Remvd);
+                    }
+                    else
+                    {
+                        if (tira == "1")
+                        {
+                            goto case "1";
+                        }
+                        else
+                        {
+                            goto case "2";
+                        }
+                        throw new ArithmeticException("Movimento Invalido");
+                    }
+                    break;
+            }
+            TorreFinal.Add(torre1);
+            TorreFinal.Add(torre2);
+            TorreFinal.Add(torre3);
+            return TorreFinal;
+        }   
     }
 }
+
+
+
