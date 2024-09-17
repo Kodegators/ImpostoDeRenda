b1a19b1023220b3df893b51ed387882c0694564b
João Otávio <joaootavioocortes@gmail.com>
2024-09-16 21:26:32 -0300

    Teste torresHanoi (Feito por Daniel Rodrigues)

---
+++ TestProject1/TorresHanoiTeste.cs
@@ -0,0 +1,38 @@
+﻿using ImpostoDeRenda.uteis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class TorresHanoiTeste
+    {
+        [TestMethod]
+        public void TorresHanoiWin()
+        {
+            var torreInicial = new List<List<int>>()
+            {
+                new List<int>{ 3, 2, 1 },
+                new List<int>(),
+                new List<int>()
+            };
+
+            var torreEsperada = new List<int> { 3, 2, 1 };
+
+            var torreFinal = TorresHanoi.MetodoConstroiTorre("1", "3", torreInicial[0], torreInicial[1], torreInicial[2]);
+            torreFinal = TorresHanoi.MetodoConstroiTorre("1", "2", torreFinal[0], torreFinal[1], torreFinal[2]);
+            torreFinal = TorresHanoi.MetodoConstroiTorre("3", "2", torreFinal[0], torreFinal[1], torreFinal[2]);
+            torreFinal = TorresHanoi.MetodoConstroiTorre("1", "3", torreFinal[0], torreFinal[1], torreFinal[2]);
+            torreFinal = TorresHanoi.MetodoConstroiTorre("2", "1", torreFinal[0], torreFinal[1], torreFinal[2]);
+            torreFinal = TorresHanoi.MetodoConstroiTorre("2", "3", torreFinal[0], torreFinal[1], torreFinal[2]);
+            torreFinal = TorresHanoi.MetodoConstroiTorre("1", "3", torreFinal[0], torreFinal[1], torreFinal[2]);
+
+            var resultado = TorresHanoi.TorresHanoiJogo(torreFinal, torreEsperada);
+
+            Assert.AreEqual("0", resultado);
+        }
+    }
+}
